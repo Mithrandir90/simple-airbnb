74316dd130697f4604b99a91dd34573430ac0087
Giuseppe Russo <russo.giu@outlook.com>
2021-11-11 00:53:06 +0000

    kind of light airbnb done

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,43 @@
+class FlatsController < ApplicationController
+  before_action :find_flats, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def show
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path(@flat)
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flats_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path(@flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flats
+    @flat = Flat.find(params[:id])
+  end
+end
